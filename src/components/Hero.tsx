335cc2d54cca7efa9fd30211c0220806baa26f0c
subhranshu <subhranshus@mindfiresolutions.com>
2025-08-21 12:59:29 +0530

    fix: Fix mobile responsive issues while scrolling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,10 +17,12 @@ import FAQ from "./FAQ"
 import Contact from "./Contact"
 import Footer from "./Footer"
 
+
 const Hero = () => {
   const ref = useRef<HTMLDivElement>(null)
   const { scrollYProgress } = useScroll({})
-  
+
+
   const [isDesktop, setIsDesktop] = useState<boolean>(window.innerWidth >= 1024)
 
   useEffect(() => {
@@ -29,22 +31,19 @@ const Hero = () => {
     return () => window.removeEventListener("resize", handleResize)
   }, [])
 
-  const bottleTopY = isDesktop
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    ? useTransform(scrollYProgress, [0, 0.185], [-50, 1150])
-    : 0
-  const bottleBottomY = isDesktop
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    ? useTransform(scrollYProgress, [0, 0.185], [-350, 400])
-    : 0
+  const bottleTopY = useTransform(scrollYProgress, [0, isDesktop ? 0.185 : 0.12], [-50, isDesktop ? 1150 : 1200])
+  const bottleBottomY = useTransform(scrollYProgress, [0, isDesktop ? 0.185 : 0.12], [-350, isDesktop ? 400 : 450])
 
   const varieties: string[] = ["Vacuum Bottles", "Fridge Bottles & Jugs", "Borosilicate Bottles", "Kettles"]
   const varieties_icon: string[] = [variety1, variety2, variety3, variety4]
 
+  useEffect(() => {
+    console.log(scrollYProgress)
+  }, [scrollYProgress])
+
   return (
     <>
       <div className="flex flex-col justify-center items-center my-6 gap-8 overflow-x-hidden" ref={ref}>
-        {/* Navbar */}
         <nav className='lg:h-[80px] flex justify-center lg:justify-between w-full lg:px-44'>
           <div className="left">
             <img src={Logo} alt="Logo" className="mt-2" />
@@ -52,7 +51,7 @@ const Hero = () => {
           <div className="right hidden md:block">
             {["Shop", "Contact Us", "About", "Journal", "Custom"].map((item: string) => {
               return (
-                <button key={item} className="mx-8 text-[20px]">{item}</button>
+                <button className="mx-8 text-[20px]">{item}</button>
               )
             })}
             <button className="bg-gradient-to-r from-[#00D1FF] to-[#1A83FF] text-white text-xl ms-14 mt-2 px-8 py-2 rounded-3xl">
@@ -60,53 +59,27 @@ const Hero = () => {
             </button>
           </div>
         </nav>
-
-        {isDesktop && (
-          <div className="absolute inset-0 top-40 z-50 -translate-y-36 lg:-translate-y-0">
-            <motion.div
-              className="bottle-top"
-              initial={{ y: 200 }}
-              animate={{ y: -50 }}
-              transition={{ duration: 2.5, delay: 1 }}
-              style={{ y: bottleTopY }}
-            >
-              <img src={BottleTop} alt="Bottle Top" className="m-auto scale-50 lg:scale-100" />
-            </motion.div>
-            <motion.div
-              className="bottle-bottom"
-              initial={{ y: -550 }}
-              animate={{ y: -350 }}
-              transition={{ duration: 2.5, delay: 1 }}
-              style={{ y: bottleBottomY }}
-            >
-              <img src={BottleBottom} alt="Bottle Bottom" className="m-auto scale-50 lg:scale-100" />
-            </motion.div>
-          </div>
-        )}
-
-        <motion.div
-          className="circle bg-gradient-to-b from-[#00D1FF] to-[#1A83FF] h-[600px] w-[600px] rounded-full p-2 absolute top-40"
-          initial={{ scale: 0.6 }}
-          animate={{ scale: 1 }}
-          transition={{ delay: 1, duration: 2 }}
-        >
-          <motion.div
-            className="bg-white rounded-full h-[582px]"
-            initial={{ scale: 0.97 }}
-            animate={{ scale: 1 }}
-            transition={{ delay: 1, duration: 2 }}
-          />
+        <div className="absolute inset-0 top-40 z-50 -translate-y-36 lg:-translate-y-0">
+          <motion.div className="bottle-top" initial={{ y: 200 }} animate={{ y: -50 }} transition={{ duration: 2.5, delay: 1 }} style={{ y: bottleTopY }}>
+            <img src={BottleTop} alt="Bottle Top" className="m-auto scale-50 lg:scale-100" />
+          </motion.div>
+          <motion.div className="bottle-bottom" initial={{ y: -550 }} animate={{ y: -350 }} transition={{ duration: 2.5, delay: 1 }} style={{ y: bottleBottomY }}>
+            <img src={BottleBottom} alt="Bottle Bottom" className="m-auto scale-50 lg:scale-100" />
+          </motion.div>
+        </div>
+        <motion.div className="circle bg-gradient-to-b from-[#00D1FF] to-[#1A83FF] h-[600px] w-[600px] rounded-full p-2 absolute top-40" initial={{ scale: 0.6 }} animate={{ scale: 1 }} transition={{ delay: 1, duration: 2 }}>
+          <motion.div className="bg-white rounded-full h-[582px]" initial={{ scale: 0.97 }} animate={{ scale: 1 }} transition={{ delay: 1, duration: 2 }}>
+          </motion.div>
         </motion.div>
-
         <div className="z-40 mt-24">
           <div className="flex flex-col items-center mt-8">
             <motion.div initial={{ opacity: 0, y: -50, scale: 0 }}
               animate={{ opacity: 1, y: 0, scale: 1 }}
               transition={{ duration: 3, delay: 1 }}>
-              <div className="text-4xl md:text-6xl lg:text-[96px] font-medium text-center leading-tight">
+              <div className="text-6xl lg:text-[96px] font-medium text-center leading-tight">
                 The Ultimate Companion <br /> for Hydration
               </div>
-              <div className="text-lg md:text-[30px] text-center">
+              <div className="text-[30px] text-center">
                 We believe in the power of hydration. <br />
                 Our mission is simple yet vital
               </div>
@@ -126,15 +99,14 @@ const Hero = () => {
             </div>
           </div>
         </div>
-
         <div className="grid grid-cols-1 lg:grid-cols-2 w-full mt-66">
           <div className="bg-[#decac1] ">
             <img src={BottleWithHAnd} alt="BottleWithHAnd" className="w-[91%] h-full" />
           </div>
           <div className="bg-[#20292C]">
-            <div className="flex flex-col justify-center items-center gap-14 h-screen">
+            <div className="flex flex-col justify-center items-center gap-4 lg:gap-14 h-screen pt-[450px] py-16 lg:py-0 lg:pt-0">
               {varieties.map((variety: string, index: number) => (
-                <Variety key={index} text={variety} icon={varieties_icon[index]} />
+                <Variety text={variety} icon={varieties_icon[index]} />
               ))}
             </div>
           </div>
@@ -149,6 +121,8 @@ const Hero = () => {
       <Contact />
       <Footer />
     </>
+
+
   )
 }
 
--- src/components/Variety.tsx
+++ src/components/Variety.tsx
@@ -7,7 +7,7 @@ interface VarietyProps {
 
 const Variety: FC<VarietyProps> = ({ text, icon }) => {
   return (
-    <div className="flex gap-10 w-sm lg:w-md items-center my-3 px-10">
+    <div className="flex gap-10 w-sm lg:w-xl items-center my-3 px-10">
       <img src={icon} alt={text} />
       <p className="text-white  text-lg lg:text-[40px]">
         {text}
